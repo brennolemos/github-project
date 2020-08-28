a2b1952ec200c851af505fe55fc241ec9a99e353
Breno Lemos <brenomiros@gmail.com>
2020-08-28 19:22:45 -0300

    Map functionality

--- src/components/User/Map.js
+++ src/components/User/Map.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import './Map.css';
+import axios from 'axios';
+import Loading from '../Loading/Loading';
 
-const Map = () => {
-  return (
-    <div className="Map box">
-      <h2>Localização</h2>
-      <div>Esse é um teste de conteúdo</div>
-    </div>
-  );
+const Location = ({ location }) => {
+  const [coord, setCoord] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchCoordinates = async () => {
+      const { data } = await axios.get(
+        `https://nominatim.openstreetmap.org/search?q=${location}&format=json`,
+      );
+      setCoord(data[0]);
+    };
+    fetchCoordinates();
+  }, [location]);
+
+  if (coord)
+    return (
+      <div className="Map box">
+        <Map center={[coord.lat, coord.lon]} zoom={11}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[coord.lat, coord.lon]} />
+        </Map>
+      </div>
+    );
+  else return <p>Renderizando a localização do usuário...</p>;
 };
 
-export default Map;
+export default Location;
