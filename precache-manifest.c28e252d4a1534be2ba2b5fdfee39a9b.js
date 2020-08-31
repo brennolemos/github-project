a0346dfdddaa3661c9587a7f50eccae6c4d08de4
Breno Lemos <brenomiros@gmail.com>
2020-08-31 14:49:01 -0300

    Updates

---
+++ precache-manifest.c28e252d4a1534be2ba2b5fdfee39a9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177dbcd44ccb209845fea65585bdbc66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4085a2cc4e97224520e",
+    "url": "/static/css/main.c1667ddb.chunk.css"
+  },
+  {
+    "revision": "3f2282d76d3fc971b6b2",
+    "url": "/static/js/2.da9bb92d.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.da9bb92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4085a2cc4e97224520e",
+    "url": "/static/js/main.5deea9d5.chunk.js"
+  },
+  {
+    "revision": "c54dc0f18f17992a735a",
+    "url": "/static/js/runtime-main.23dbea3b.js"
+  },
+  {
+    "revision": "7b93979d9f2b726b9992368ec23b632d",
+    "url": "/static/media/github.7b93979d.svg"
+  },
+  {
+    "revision": "2ddf0e582d4eaa828ee46cbcdd7cc8f4",
+    "url": "/static/media/search.2ddf0e58.svg"
+  }
+]);
--- src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'https://api.github.com/users';
-const api = axios.create({ baseURL: API_URL });
-
-export default api;
