3081cac9eb9646476800aa10e159c0df2fe9bddf
Breno Lemos <brenomiros@gmail.com>
2020-08-31 14:55:30 -0300

    Updates

---
+++ precache-manifest.4b88e319d9a57e93ab7add8a4a3b8d13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdf9dbcec9ed16e8d4816b0fcaace35",
+    "url": "/github-project/index.html"
+  },
+  {
+    "revision": "a8ef974a041694d47d4d",
+    "url": "/github-project/static/css/main.977c809f.chunk.css"
+  },
+  {
+    "revision": "3f2282d76d3fc971b6b2",
+    "url": "/github-project/static/js/2.da9bb92d.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/github-project/static/js/2.da9bb92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ef974a041694d47d4d",
+    "url": "/github-project/static/js/main.5deea9d5.chunk.js"
+  },
+  {
+    "revision": "791d9a739a085809fc5d",
+    "url": "/github-project/static/js/runtime-main.3dfb4a83.js"
+  },
+  {
+    "revision": "7b93979d9f2b726b9992368ec23b632d",
+    "url": "/github-project/static/media/github.7b93979d.svg"
+  },
+  {
+    "revision": "2ddf0e582d4eaa828ee46cbcdd7cc8f4",
+    "url": "/github-project/static/media/search.2ddf0e58.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c28e252d4a1534be2ba2b5fdfee39a9b.js"
+  "/github-project/precache-manifest.4b88e319d9a57e93ab7add8a4a3b8d13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
